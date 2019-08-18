8522563ca532e679f8ce0ccebb21b100cd3bb4e6
Vlad <techiedesu@protonmail.com>
2019-08-18 22:48:23 +0700

    Implemented base project structure (resolved #4) and added swagger (resolved #3).

---
+++ src/Libraries/Sumire.Common/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sumire.Common
+{
+    public static class StringExtensions
+    {
+        public static void DropBlacklistedChars(this string self)
+        {
+            self.Normalize();
+        }
+    }
+
+    public static class GuidExtensions
+    {
+        public static bool IsEmpty(this Guid self)
+        {
+            return self == Guid.Empty;
+        }
+    }
+}
---
+++ src/Libraries/Sumire.Core/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Sumire.Core
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
